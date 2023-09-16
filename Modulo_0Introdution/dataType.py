c778fcae91ebdf27dbf5836cbc83c5d4919ed82d
CarlosMandele <104703594+CarlosMandele@users.noreply.github.com>
2023-09-16 20:28:29 -0300

    added files

---
+++ Modulo_0Introdution/dataType.py
@@ -0,0 +1,20 @@
+# Tipos de Dados Primitivos em Python
+
+
+# Inteiro
+idade = 20
+
+#Float(Fracionarios)
+altura = 1.75
+
+# String(caractere)
+saudacao = "Olá mundo!"
+
+# Lógico(booleano)
+aprovado = True
+
+# Função type recebe como argumento uma variavel e informa qual o tipo esta armazenado na variavel
+print(type(idade))
+print(type(altura))
+print(type(saudacao))
+print(type(aprovado))
---
+++ Modulo_0Introdution/entrada_data_funtion_Input.py
@@ -0,0 +1,10 @@
+# informando dados com a função input()
+
+# A caracteristica da função input() é receber um valor e converter para uma string 
+n1 = input("Digite a primeira variavel:")
+n2 = input("Digite a segunda variavel:")
+
+
+#Variavel soma
+soma = n1 + n2
+print(soma)
