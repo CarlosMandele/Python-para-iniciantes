615eca0e3ff3f4f9fcbe5d1182e904639dabae48
CarlosMandele <104703594+CarlosMandele@users.noreply.github.com>
2023-09-16 21:30:38 -0300

    Update file

---
+++ Modulo_0Introdution/conversion_data.py
@@ -0,0 +1,13 @@
+# CONVESRÃO DE DADOS EM PYTHON
+
+# A função int recebe uma string e converte para um valor do tipo inteiro
+a = int (input("Digite a primeira variavel:")) # 5 ---> "5"
+b = int (input("Digite a segunda variavel:")) # 7 ---> "7"
+x = float (input("Digite a terceira variavel:"))
+
+soma = a + b #"5" + "7"
+dobro = x * 3 # "2.5"
+
+print(soma)
+print(dobro)
+
