2972ccd27a2becb1bfeade70db783b1f8234b98b
CarlosMandele <104703594+CarlosMandele@users.noreply.github.com>
2023-09-26 22:31:08 -0300

    Add arquivo operadoresaatribuittion

---
+++ Modulo02/operedoresaatribuition.py
@@ -0,0 +1,13 @@
+# Operador"="
+nome = "Pedro"
+nome == "Pedro"
+
+# 1) - Exemplos
+x = 10
+x += 2  # x = x + 2
+x -= 2  # x = x - 2
+x *= 2  # x = x * 2
+x /= 2  # x = x / 2
+x //= 2 # x = x // 2
+x **= 2 # x = x ** 2
+x %= 2 # x = x % 2
