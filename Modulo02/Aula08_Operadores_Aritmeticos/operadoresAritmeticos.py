1dd5646a847f959b336c3b7a826c5e21cdb4b67b
CarlosMandele <104703594+CarlosMandele@users.noreply.github.com>
2023-09-26 21:39:40 -0300

    Add mais códigos

---
+++ Modulo02/Aula08_Operadores_Aritmeticos/operadoresAritmeticos.py
@@ -0,0 +1,20 @@
+# Adição
+print(3+2)
+
+# Subtração
+print(3-2)
+
+# Divisão
+print(3/2)
+
+# Multiplicação
+print(3*2)
+
+# Coeciente
+print(3//3)
+
+# Resto
+print(3%2)
+
+# Potencia
+print(3**2)
