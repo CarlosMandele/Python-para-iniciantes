f0ebdde5c508c539e4bb865b04bb9519bf597e2e
CarlosMandele <104703594+CarlosMandele@users.noreply.github.com>
2023-09-27 00:32:54 -0300

    Add arquivo quiz

---
+++ Modulo03_EstrturasCondicionais/quiz.py
@@ -0,0 +1,29 @@
+# 1) - Analise o seguintte código abaixo e responda:
+
+# a) - Após a estrutura condicional encadeada, será exibido em tela:
+a = 1
+b = -3
+c = -10
+
+delta = b * b - 4 * a * c
+if (delta > 0):
+    print("#1")
+elif (delta == 0):
+    print("#2")
+elif (delta < 0):
+    print("#3")
+else:
+    print("#4")
+
+#### ---------------------  
+
+    
+# b) - Analise o código abaixo e responda: 
+# Após a estrutura condicional composta,
+# o valor da variável x será:
+x = 3
+y = 9 
+if (y**(1/2)== x):
+    x += 1
+else:
+    x += 2
