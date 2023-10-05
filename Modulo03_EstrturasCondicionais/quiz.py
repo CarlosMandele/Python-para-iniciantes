47f5f2880698db854ffa3b3688cf267cf9c84fc9
CarlosMandele <104703594+CarlosMandele@users.noreply.github.com>
2023-10-05 17:14:07 -0300

    Add Modulo04

--- Modulo03_EstrturasCondicionais/quiz.py
+++ Modulo03_EstrturasCondicionais/quiz.py
@@ -6,9 +6,9 @@ b = -3
 c = -10
 
 delta = b * b - 4 * a * c
-if (delta > 0):
+if delta > 0:
     print("#1")
-elif (delta == 0):
+elif delta == 0:
     print("#2")
 elif (delta < 0):
     print("#3")
---
+++ Modulo04_EstruturasRepetition/function_range.py
@@ -0,0 +1,6 @@
+# Função range() - diferentes formatos para escrever a função
+
+# 1)  - range(x):
+range(5)
+ 
+
